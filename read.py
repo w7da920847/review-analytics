6f388991019a9822b8dea935ca39caeb6ca659ee
gary <w7da920847@gmail.com>
2023-10-01 11:44:00 +0800

    add basic

---
+++ read.py
@@ -0,0 +1,12 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f :
+	for line in f :
+		count = count +1
+		data.append(line)
+		if count % 1000 == 0 :
+			print(len(data))
+print(len(data))
+print(data[0])
+print('-------------')
+print(data[1])
